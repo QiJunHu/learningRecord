ef488cc0ea44be3274cd5cc44c826a4fdf10f35a
QiJunHu <xiaoyaohuqijun@sina.com>
2015-11-29 23:51:24 +0800

    Update functionObejectAndAlgorithm.cpp
    add Content about std::functional and function adapter

--- functionObejectAndAlgorithm.cpp
+++ functionObejectAndAlgorithm.cpp
@@ -70,6 +70,66 @@ int main()
     std::cout<<cn<<std::endl;
     std::vector<int>::size_type cn1 = std::count_if(ivec.begin(),ivec.end(),equal_cls(10));
     std::cout<<cn1<<std::endl;
+    
+    
+    
+    //use std::functional  and function adapter
+    
+    
+    std::vector<int> ivec;
+    ivec.reserve(1030);
+    for(int i = 0 ; i <1030 ;++i)
+    {
+        ivec.push_back(i);
+    }
+
+
+    //find all value that is greater than 1024
+    std::vector<int>::iterator result = std::find_if(ivec.begin() ,  ivec.end() , std::bind2nd(std::greater<int>() ,1024) );
+
+    while(result != ivec.end())
+    {
+        std::cout<<*result<<std::endl;
+        result = std::find_if( ++result ,  ivec.end() , std::bind2nd(std::greater<int>() ,1024) );
+    }
+
+
+
+    std::vector<std::string> svec;
+    svec.push_back("hello world");
+    svec.push_back("test");
+    svec.push_back("huqijun");
+    svec.push_back("pooh");
+
+    //find all value that is not equal to "pooh"
+    std::vector<std::string>::iterator ret = std::find_if(svec.begin(),svec.end(),std::bind2nd(std::not_equal_to<std::string>(),"pooh") );
+    while(ret != svec.end())
+    {
+        std::cout<<*ret<<std::endl;
+        ret = std::find_if(++ret,svec.end(),std::bind2nd(std::not_equal_to<std::string>(),"pooh") );
+    }
+
+
+    //double every value
+    std::vector<int> ivec2;
+    ivec2.reserve(12);
+    for(int i = 0; i<12 ; ++i)
+    {
+        ivec2.push_back(i);
+    }
+
+    std::for_each(ivec2.begin(),ivec2.end(),std::bind2nd(std::multiplies<int>(),2) );
+
+    for(std::vector<int>::iterator i = ivec2.begin() ;  i!= ivec2.end()  ; ++i)
+    {
+        std::cout<<*i<<std::endl;
+    }
+    
+    
+    
+    
+    
+    
     return 0;
 
 }
